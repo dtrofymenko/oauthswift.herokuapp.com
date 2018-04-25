e1178dbc0fb00a48b5bbfc11dcfd37f35a8bce5e
Dmytro Trofymenko <d.trofymenko@wnb.com.ua>
2018-04-25 15:20:55 +0300

    Added ability to specify scheme

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -3,7 +3,7 @@
 get "/callback/:scheme" do
   path = request.path
   path_query = "#{path}?#{request.query_string}"
-  url = "#{scheme}://oauth-callback#{path_query}"
+  url = "#{:scheme}://oauth-callback#{path_query}"
   puts url
   redirect url
 end
