91ed35cb449dc8ebc0a71789c6ab58622852c46f
Dmytro Trofymenko <d.trofymenko@wnb.com.ua>
2018-06-12 16:40:08 +0300

    Try / instead of //

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -4,7 +4,7 @@ get "/callback/:scheme" do
   scheme = params[:scheme]
   path = request.path.gsub("/#{scheme}","")
   path_query = "#{path}?#{request.query_string}"
-  url = "#{scheme}://#{path_query}"
+  url = "#{scheme}:/#{path_query}"
   puts url
   redirect url
 end
